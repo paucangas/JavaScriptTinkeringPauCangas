cb5c155fd1b1f05f8350905878d18252b79bce35
«Pau <paucangas@iesebre.com>
2024-11-05 19:52:08 +0100

    Projecte

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/',
   plugins: [vue()],
   define: {
     'import.meta.env.VITE_API_URL': JSON.stringify('https://api.example.com/data')
